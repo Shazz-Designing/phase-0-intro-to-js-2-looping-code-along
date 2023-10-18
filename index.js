1c988d65d8d74dadbfcb171de20f6026aaa283f8
sharon <sapot@safaricom.co.ke>
2023-10-18 21:19:59 +0300

    Add code to index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+// Create a function to write thank you cards for an array of names
+function writeCards(names, eventName) {
+    const thankYouCards = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouCards;
+  }
+  
+  // Create a function to count down from a positive integer to zero
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Example usage
+  writeCards(["Charlie", "Samip", "Ali"], "birthday");
+  countDown(10);
+  
